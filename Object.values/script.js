23a0eac9182032c4bcc90c0917f9023f7da39bcb
Erme91 <locastroerme@gmail.com>
2024-11-11 13:11:22 +0100

    Values

---
+++ Object.values/script.js
@@ -0,0 +1,10 @@
+let student = {
+    name: "Ermelinda",
+    age: "32",
+    grade: "Philosophy degree",
+    school: "University of Perugia"
+}
+
+Object.values (student)
+
+console.log (Object.values(student))
