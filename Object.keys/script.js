05d860061424863000919feaada96ef5181eda03
Erme91 <locastroerme@gmail.com>
2024-11-11 13:03:17 +0100

    Keys

---
+++ Object.keys/script.js
@@ -0,0 +1,9 @@
+let student = {
+    name: "Ermelinda",
+    age: "32",
+    grade: "Philosophy degree",
+    school: "University of Perugia"
+}
+
+Object.keys (student)
+console.log (Object.keys(student))
