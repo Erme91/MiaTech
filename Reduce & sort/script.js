310a5d65b1493f2d33885eaec062ecd95c123399
Erme91 <locastroerme@gmail.com>
2024-11-17 17:10:43 +0100

    Reduce and Sort methods

---
+++ Reduce & sort/script.js
@@ -0,0 +1,9 @@
+let expenses = [10, 35, 26, 53, 62];
+let sum = expenses.reduce((acc, i)=>{return acc + i});
+
+console.log(sum)
+
+let words = ["banana", "apple", "cherry", "date"];
+let newWords = words.sort();
+
+console.log(newWords)
