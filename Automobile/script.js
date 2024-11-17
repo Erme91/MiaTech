da62b12e7b0ab415160fcfb6f9c1a1729eac1fb0
Erme91 <locastroerme@gmail.com>
2024-11-17 17:49:51 +0100

    Automobile

---
+++ Automobile/script.js
@@ -0,0 +1,13 @@
+class Automobile {
+    marca = ""
+    modello = ""
+    anno = ""
+
+    constructor(marca, modello, anno){
+        this.marca = marca
+        this.modello = modello
+        this.anno = anno
+    }
+}
+
+console.log(Symbol("Lancia Y, GPL Eco-Chic, 2015").description)
