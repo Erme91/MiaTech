111eb59858f70c61de1695f771b45041f0033a46
Erme91 <locastroerme@gmail.com>
2024-11-20 20:25:38 +0100

    Console.log

---
+++ Uso console.log/script.js
@@ -0,0 +1,10 @@
+let num = 55;
+let str = "Il compito principale nella vita di un uomo è di dare alla luce se stesso trasformandosi in tutto ciò che è in grado di essere. Il risultato di tali sforzi sarà la sua personalità. E.Fromm";
+let arr = [28, 35, 88, 110];
+let square = {
+    color: "violet",
+    widht: 50,
+    height: 50
+};
+
+console.log(num, str, arr, square)
