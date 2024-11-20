15d8f22d8a453a0ba1c271b7466c6d9061824b60
Erme91 <locastroerme@gmail.com>
2024-11-20 12:20:23 +0100

    Proprietà

---
+++ Estrazione proprietà/script.js
@@ -0,0 +1,10 @@
+let table = {
+    color: "mahogany",
+    material: "wood",
+    shape: "rectangle",
+    style: "classic"
+};
+
+let {material: second, style: fourth} = table
+
+console.log(second, fourth)
