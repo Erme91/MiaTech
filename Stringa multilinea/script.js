ddfb63d087c877e4cefaabf24c399294cd14adc4
Erme91 <locastroerme@gmail.com>
2024-11-20 20:08:40 +0100

    Stringa multilinea

---
+++ Stringa multilinea/script.js
@@ -0,0 +1,20 @@
+let identità = 
+`"Lidia"
+ "Valenza"
+  20
+ "Mazara"
+ `;
+
+let nome = "Lidia";
+let cognome = "Valenza";
+let età = 20;
+let città = "Mazara";
+
+let newIdentità = `Presentiamo la nuova studentessa iscritta al corso:
+${nome}
+${cognome}
+di anni ${età}
+e proveniente dalla città di ${città}
+`;
+
+console.log(newIdentità)
