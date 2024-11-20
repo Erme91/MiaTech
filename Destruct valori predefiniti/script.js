34abba4d51995d10ee45d1f2b0a62543a81beba5
Erme91 <locastroerme@gmail.com>
2024-11-20 13:17:33 +0100

    Valori mancanti

---
+++ Destruct valori predefiniti/script.js
@@ -0,0 +1,11 @@
+let book = {
+    genre: "adventure",
+    title: "Le tigri di Mompracem",
+    author: "",
+    year: "",
+    protagonist: ""
+};
+
+let {author, year, protagonist} = {author: "Emilio Salgari", year: 1900, protagonist: "Sandokan"};
+
+console.log(author, year, protagonist)
