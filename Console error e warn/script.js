19f8c8feaad1c8b141abbd9673b50a97eda96ff8
Erme91 <locastroerme@gmail.com>
2024-11-21 14:44:10 +0100

    Console error and warn

---
+++ Console error e warn/script.js
@@ -0,0 +1,11 @@
+const age = 145;
+if (age > 120) {
+  console.error('Attenzione! Inserire un valore realistico')
+};
+
+const età = 15;
+if (età < 18) {
+    console.warn('Attenzione! La persona ha meno anni di quelli richiesti')
+}
+
+
