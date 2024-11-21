9106ff16a6ab57ceba1f75b7b1a68242a1c4a181
Erme91 <locastroerme@gmail.com>
2024-11-21 12:58:34 +0100

    Function with template

---
+++ Funzione e template/script.js
@@ -0,0 +1,9 @@
+function persona() {
+    let persona = "Licia";
+    let lavoro = "Croazia";
+    return `Il suo nome è ${persona} e lavora in ${lavoro}`;
+};
+
+const objPerson = persona
+console.log(objPerson())
+
