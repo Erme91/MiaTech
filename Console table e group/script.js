59738c14fddf5cacba5037664e604ba62dae9210
Erme91 <locastroerme@gmail.com>
2024-11-21 15:25:49 +0100

    Console table and group

---
+++ Console table e group/script.js
@@ -0,0 +1,4 @@
+console.table({'nome': 'Vittorio', 'professione': 'studente', 'età': 15, 'città': 'Cosenza'});
+
+console.group("Benvenuti a Perugia, la città del cioccolato.", "Il tour comincerà da Piazza IV Novembre.");
+console.group("Si visiterà la cattedrale per poi procedere lungo Corso Vannucci.")
