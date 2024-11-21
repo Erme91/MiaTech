a3089102dc703e7b88b83876510f5611c77156ff
Erme91 <locastroerme@gmail.com>
2024-11-21 19:57:56 +0100

    Modified

--- Metodi & proprietà/script.js
+++ Metodi & proprietà/script.js
@@ -1,32 +1,36 @@
 class Automobile {
-    marca = "Lancia Y"
-    modello = "GPL Eco-Chic"
-    anno = "2015"
-    chilometraggio = 65000
 
+    marca = "";
+    modello = "";
+    anno = 0;
+    chilometraggio = 0;
+    
     constructor(marca, modello, anno, chilometraggio) {
         this.marca = marca
         this.modello = modello
         this.anno = anno
         this.chilometraggio = chilometraggio
-    }
+    };
+
+    descrizione(){
+        return this.marca + "" + this.modello + "" + this.anno + "" + this.chilometraggio + "km";
+    };
+
+    aggiungiChilometri(km) {
+       return this.chilometraggio = this.chilometraggio + km
+    };
+
+    mostraChilometraggio() {
+        return this.chilometraggio + "km"
+    };
 }
 
-Automobile.aggiungiChilometri = function () {
-    let km = 65000;
-    return {
-        increment: function () {
-            km++
-            return km
-        }
-    }
+let auto1 = new Automobile("Lancia", "GPL", 2015, 30000);
 
-}
+console.log(auto1.descrizione());
+      
+auto1.aggiungiChilometri(100);
 
-const count = Automobile.aggiungiChilometri()
+console.log(auto1.descrizione());
 
-Automobile.mostraChilometraggio = function () {
-    console.log(count.increment() + 100)
-}
-
-Automobile.mostraChilometraggio()
+console.log(auto1.mostraChilometraggio())
