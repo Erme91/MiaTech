56c3724c2b17a5c1b60498d1f5fccfb0e86c8648
Erme91 <locastroerme@gmail.com>
2024-11-23 00:22:06 +0100

    Saluti

---
+++ Saluta/script.js
@@ -0,0 +1,69 @@
+class Automobile {
+
+    marca = "";
+    modello = "";
+    anno = 0;
+    chilometraggio = 0;
+    
+    constructor(marca, modello, anno, chilometraggio) {
+        this.marca = marca
+        this.modello = modello
+        this.anno = anno
+        this.chilometraggio = chilometraggio
+    };
+
+    descrizione(){
+        return this.marca + "" + this.modello + "" + this.anno + "" + this.chilometraggio + "km";
+    };
+
+    aggiungiChilometri(km) {
+       return this.chilometraggio = this.chilometraggio + km
+    };
+
+    mostraChilometraggio() {
+        return this.chilometraggio + "km"
+    };
+
+    saluta() {
+        return "Salve a tutti! Vi proponiamo in concessionaria un nuovo arrivo" + this.marca + "del modello" + this.modello;
+    }
+};
+
+class Elettrica extends Automobile {
+    autonomia = 0;
+
+    constructor(marca, modello, anno, chilometraggio, autonomia){
+        super(marca, modello, anno, chilometraggio);
+        this.autonomia = autonomia;
+    }
+
+    descrizioneElettrica(){
+        return super.descrizione + this.autonomia + "km";
+    }
+
+    ricarica(km) {
+        this.autonomia += km;
+        return this.autonomia
+    };
+};
+
+let auto1 = new Automobile("Lancia", "GPL", 2015, 30000);
+
+console.log(auto1.descrizione());
+      
+auto1.aggiungiChilometri(100);
+
+console.log(auto1.descrizione());
+
+console.log(auto1.mostraChilometraggio());
+
+console.log(auto1.saluta());
+
+let vettura = new Elettrica("Ford", "Puma", 2015, 30000, 1000);
+
+console.log(vettura);
+
+console.log('La vettura è stata ricaricata. Adesso ha autonomia di:' + vettura.ricarica(300) + 'km')
+
+console.log(vettura.saluta())
+
