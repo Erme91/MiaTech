de446cac2ea3f839404e7d51d3f1bfd3852291d5
Erme91 <locastroerme@gmail.com>
2024-11-27 13:06:29 +0100

    Modified

--- Destruct valori predefiniti/script.js
+++ Destruct valori predefiniti/script.js
@@ -1,11 +1,8 @@
 let book = {
-    genre: "adventure",
-    title: "Le tigri di Mompracem",
-    author: "",
-    year: "",
-    protagonist: ""
+    genre: 'adventure',
+    title: 'Le tigri di Mompracem',
 };
 
-let {author, year, protagonist} = {author: "Emilio Salgari", year: 1900, protagonist: "Sandokan"};
+let {author = 'Emilio Salgari', year = 1900, protagonist = 'Sandokan'} = book;
 
-console.log(author, year, protagonist)
+console.log(`Autore: ${author}`, `Anno di stampa: ${year}`, `Protagonista del romanzo: ${protagonist}`)
