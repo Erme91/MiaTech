6d9aa4cbda71d7e4d2ebcf1b9ba7fdc7b16c8fcf
Erme91 <locastroerme@gmail.com>
2024-12-07 14:00:22 +0100

    Aggiunto bottone

---
+++ Modifica HTML/script.js
@@ -0,0 +1,7 @@
+const testo = document.getElementById("text");
+
+const newElement = document.createElement("button");
+
+newElement.innerHTML = `Iscriviti adesso!`;
+
+testo.appendChild(newElement)
