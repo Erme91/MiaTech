aa82c1c93ebdd0ad8833f3c40a5df96d1717cb8a
Erme91 <locastroerme@gmail.com>
2024-12-07 13:01:53 +0100

    Paragrafo modificato

---
+++ Modifica paragrafo/script.js
@@ -0,0 +1,3 @@
+const paragrafo = document.getElementsByTagName("p")[0];
+
+paragrafo.innerText = "A PERUGIA IL CIOCCOLATO… E' SEMPRE DI SCENA! CASA DEL CIOCCOLATO PERUGINA Museo - Scuola del Cioccolato - Shop - TI ASPETTA PER VIVERE UN'ESPERIENZA UNICA ALL'INSEGNA DEL CIOCCOLATO!"
