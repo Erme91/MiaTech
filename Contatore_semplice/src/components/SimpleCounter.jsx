25c333e96f81a1616304caf0627c7770365ad9e8
Erme91 <locastroerme@gmail.com>
2025-01-09 17:09:28 +0100

    Code updated with buttons

--- Contatore_semplice/src/components/SimpleCounter.jsx
+++ Contatore_semplice/src/components/SimpleCounter.jsx
@@ -3,13 +3,25 @@ import { useState } from "react";
 const SimpleCounter = () => {
     const [count, setCount] = useState(0);
     const addNumber = () => {
-        setCount(0);
+        setCount(count + 1);
+    }
+
+    const Decrement = () => {
+        if (count != 0){
+            setCount(count - 1)
+        }
+    }
+
+    const Reset = () => {
+        setCount(0)
     }
 
     return(
         <>
         <div>Count: {count}</div>
         <button onClick={addNumber}>Click!</button>
+        <button onClick={Decrement}>Decrement</button>
+        <button onClick={Reset}>Reset</button>
         </>
     )
 }
