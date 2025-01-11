6394580e9f4f85629ae6f911687a77baa8a9e746
Erme91 <locastroerme@gmail.com>
2025-01-11 17:54:07 +0100

    Childrens' content

---
+++ Props_children/src/App.jsx
@@ -0,0 +1,19 @@
+import Card from "./components/Card";
+
+function App() {
+
+  return (
+    <>
+      <Card>
+        <h2>Cappuccetto Rosso</h2>
+        <p>Cappuccetto Rosso è una delle favole europee più popolari al mondo, la protagonista è appunto
+          Cappuccetto Rosso, una bambina così chiamata perché indossa sempre una mantellina con un cappuccio rosso
+          che le è stata regalata dalla nonna.</p>
+        <br />
+        <span>Autore: Charles Perrault, 1697</span>
+      </Card>
+    </>
+  )
+}
+
+export default App
