1e86b5bac44b90dcb6f23c566b142198c525f2f7
Erme91 <locastroerme@gmail.com>
2025-01-17 20:29:33 +0100

    Created useFetch hook

---
+++ Crea_useFetch/src/App.jsx
@@ -0,0 +1,40 @@
+import useFetch from "./hooks/useFetch";
+
+const App = () => {
+  const { data: posts, loading, error } = useFetch('https://jsonplaceholder.typicode.com/posts');
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>user id</th>
+            <th>id</th>
+            <th>title</th>
+            <th>body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts && posts.map(post => (
+            <tr key={post.id}>
+                <td>{post.userId}</td>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default App;
