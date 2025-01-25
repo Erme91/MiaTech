005f8615b08046a93777c362a0be312024661f9b
Erme91 <locastroerme@gmail.com>
2025-01-25 13:11:23 +0100

    Added a public layout

--- Rotte_di_base/src/App.jsx
+++ Rotte_di_base/src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
-import Navbar from './components/Navbar';
+import PublicLayout from './layouts/PublicLayout';
 
 const App = () => {
   return (
     <>
-    <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />}/>
+        <Route path='/' element={<PublicLayout />}>
+          <Route path='' element={<Home />} />
+          <Route path='about' element={<About />} />
+        </Route>
       </Routes>
     </>
   )
