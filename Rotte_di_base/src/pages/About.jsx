b65044903067a685fb2d93decafbb4df79048426
Erme91 <locastroerme@gmail.com>
2025-01-25 13:47:38 +0100

    Added navigation button

--- Rotte_di_base/src/pages/About.jsx
+++ Rotte_di_base/src/pages/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = () => {
+        navigate("/")
+    }
+
     return (
         <>
             <h2>About</h2>
             <p>La pagina Home mostra una lista di cose da fare.</p>
+            <button onClick={handleNavigation}>Return to Home</button>
         </>
     )
 }
