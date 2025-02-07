b7dd52489babaf2a7c8d83da52d5618ae56d6569
Erme91 <locastroerme@gmail.com>
2025-02-07 17:08:13 +0100

    Add todo and User

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,3 +1,16 @@
 import { Todo } from "./types";
 
-const todos: Todo[] = []
+const todos: Todo[] = [];
+
+let newId = 1;
+
+function addTodo(title: string): void {
+    const newTodo: Todo = {
+        id: newId++,
+        title: title,
+        completed: false
+    }
+    todos.push(newTodo)
+}
+
+addTodo("List")
