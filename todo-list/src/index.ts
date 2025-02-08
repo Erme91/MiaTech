a0ab037e09fdf046ae2a35fee64beae441e6ca7b
Erme91 <locastroerme@gmail.com>
2025-02-08 16:28:58 +0100

    tuple

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -67,3 +67,7 @@ function updateTodo (id: number, update: Partial<Todo>): void {
 }
 
 updateTodo(2, {title: "Funzioni"});
+
+function getTodoSummary(todo: Todo) : [string, boolean] {
+    return [todo.title, todo.completed]
+};
