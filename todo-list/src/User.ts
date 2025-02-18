a86aaafd28b3e6fcc33fd52205b778a7a5268794
Erme91 <locastroerme@gmail.com>
2025-02-18 19:24:28 +0100

    Modified class User and added addTodo

--- todo-list/src/User.ts
+++ todo-list/src/User.ts
@@ -1,11 +1,27 @@
+class Todo {
+    title: string;
+    completed: boolean;
+
+    constructor(title: string, completed: boolean = false) {
+        this.title = title;
+        this.completed = completed;
+    }
+}
+
 class User {
     id: number;
     name: string;
     email?: string;
+    todos: Todo[];
 
-    constructor() {
-        this.id= 1;
-        this.name= "Claudio";
-        this.email= "claudio.11@gmail.com"
+    constructor(id: number, name: string, email?: string) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.todos = [];
+    }
+
+    addTodo(todo: Todo): void {
+        this.todos.push(todo);
     }
 }
