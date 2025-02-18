29a0cfef123385c3ea6d2310b6d57db0174d2806
Erme91 <locastroerme@gmail.com>
2025-02-18 19:15:54 +0100

    Created class User

---
+++ todo-list/src/User.ts
@@ -0,0 +1,11 @@
+class User {
+    id: number;
+    name: string;
+    email?: string;
+
+    constructor() {
+        this.id= 1;
+        this.name= "Claudio";
+        this.email= "claudio.11@gmail.com"
+    }
+}
