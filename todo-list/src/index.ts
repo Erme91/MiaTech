5994e86a2781f250021ccda0374a908e098ade78
Erme91 <locastroerme@gmail.com>
2025-02-18 19:30:03 +0100

    Utilizzare classe User in index.ts

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,30 +1,42 @@
 import { Todo, TodoStatus } from "./types";
 
-const todos: Todo[] = []
+class User {
+    id: number;
+    name: string;
+    email?: string;
+    todos: Todo[];
 
+    constructor(id: number, name: string, email?: string) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.todos = [];
+    }
+
+    addTodo(todo: Todo): void {
+        this.todos.push(todo);
+    }
+}
+
+const todos: Todo[] = [];
 let newId = 1;
 
-function addTodo(title: string): void {
+function addTodoToUser(user: User, title: string): void {
     const newTodo: Todo = {
         id: newId++,
         title,
         completed: false,
         status: TodoStatus.Pending
-    }
-
-    todos.push(newTodo)
+    };
+    user.addTodo(newTodo);
 }
 
-addTodo("Added new Todo");
+function updateTodoStatus(todoId: number, status: TodoStatus): void {
+    const todo = todos.find(t => t.id === todoId);
 
-function updateTodoStatus(todoId: number, status: TodoStatus) : void {
-    const todo= todos.find(t => t.id === todoId)
-
-    if(todo) {
-        todo.status = status
+    if (todo) {
+        todo.status = status;
     } else {
-        console.log("Nessun todo trovato")
+        console.log("Nessun todo trovato");
     }
 }
-
-updateTodoStatus(1, TodoStatus.Completed)
